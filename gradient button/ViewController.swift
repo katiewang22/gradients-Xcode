dfa0d9f3b0b8bf9adb544662e5154af96ab217bd
sugataekookie <57153170+sugataekookie@users.noreply.github.com>
2020-06-05 10:40:17 -0400

    Initial Commit

---
+++ gradient button/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gradient button
+//
+//  Created by Quan Wang on 6/5/20.
+//  Copyright © 2020 Katie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
