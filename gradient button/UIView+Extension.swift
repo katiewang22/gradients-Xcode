3c71b25a2fcd7779e7bb83ff0c11f06dfd324bbc
Katie Wang <katie.wang1950f@gmail.com>
2020-06-17 20:11:50 -0400

    gradients

---
+++ gradient button/UIView+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  gradientButton.swift
+//  gradient button
+//
+//  Created by Quan Wang on 6/9/20.
+//  Copyright © 2020 Katie Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
--- gradient button/ViewController.swift
+++ gradient button/ViewController.swift
@@ -6,15 +6,30 @@
 //  Copyright © 2020 Katie Wang. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myButton.layer.cornerRadius = myButton.frame.size.height/2
+        myButton.layer.masksToBounds = true
+        
+        let blueColor = UIColor(red: 0.1686, green: 0.651, blue: 1, alpha: 1.0)
+        let pinkColor = UIColor(red: 1, green: 0.6078, blue: 0.8078, alpha: 1.0)
+        let whiteColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+        let hotPink = UIColor(red: 1, green: 0.2667, blue: 0.451, alpha: 1.0)
+        let orangeColor = UIColor(red: 1, green: 0.7137, blue: 0.1882, alpha: 1.0)
+        let redColor = UIColor(red: 1, green: 0.2784, blue: 0.2667, alpha: 1.0)
+        
+        view.setGradientBackground(colorOne: blueColor, colorTwo: pinkColor)
+        myView.setGradientBackground(colorOne: hotPink, colorTwo: whiteColor)
+        myButton.setGradientBackground(colorOne: orangeColor, colorTwo: redColor)
     }
-
-
 }
 
